ff2597133ff67c273e62823038b68f459f4999c7
Brekke-Green <brekke.andrew.green@gmail.com>
2023-12-03 15:21:42 -0500

    Successfully complete AOC day02 with Go

--- year_2023/day02.go
+++ year_2023/day02.go
@@ -19,37 +19,43 @@ func main() {
 		"blue":		14,
 	}
 
-	dataArr := strings.Split(string(data), "Game ")
+	data_string := strings.ReplaceAll(string(data), "\n", "")
+	dataArr := strings.Split(data_string, "Game ")
 	dataArr = dataArr[1:]
 
 
 	realGames := 0
+	badGamesMap := make(map[int]bool)
 	dataMap := make(map[int][]string)
 
 	for i, line := range dataArr {
 		cleaned := strings.SplitAfter(line, ": ")
 		games := strings.Split(cleaned[1], "; ")
-		dataMap[i] = games 
+		dataMap[i+1] = games 
 	}
+	
 
 
 	for num, games := range dataMap {
 		for _, game := range games {
-			flag := true
-			cube := strings.Split(game, ",")
+			cube := strings.Split(game, ", ")
 			for _, c := range cube {
 				die := strings.Split(c, " ")
+				die[1] = strings.ReplaceAll(die[1], "\n", "")
 				n, err := strconv.Atoi(die[0])
 				if err != nil {
-					os.Exit(1)
+					panic(err)
 				}
 				if cubes[die[1]] < n {
-					flag = false
+					badGamesMap[num] = true
 				}
 			}
-			if flag {
-				realGames+=num
-			}
+		}
+	}
+
+	for i := 1; i <= 100; i++ {
+		if _, found := badGamesMap[i]; found == false {
+			realGames+=i
 		}
 	}
 
