a0a9f9baf309376cd376bc71e7de262936eb6e57
Brekke-Green <brekke.andrew.green@gmail.com>
2023-12-08 14:41:24 -0500

    Successfully complete AOC day06 part II with Go

--- year_2023/day06.go
+++ year_2023/day06.go
@@ -5,19 +5,21 @@ import (
 )
 
 type Doc struct {
-	time	[]int
-	dist	[]int
+	time	[]int64
+	dist	[]int64
 }
 
 func main() {
-	//sample := Doc{time:	[]int{7, 15, 30}, dist: []int{9, 40, 200}}
-	input := Doc{time:	[]int{59, 70, 78, 78}, dist: []int{430, 1218, 1213, 1276}}
+	//sample := Doc{time:	[]int64{71530}, dist: []int64{940200}}
+	input := Doc{time: []int64{59707878}, dist: []int64{430121812131276}}
 
-	var results []int
+	var results []int64
 
 	for i, t := range input.time {
-		wins := 0
-		for j := 1; j < t; j++ {
+		var wins int64
+		wins = 0
+		var j int64
+		for j = 1; j < t; j++ {
 			d := j * (t - j)
 			if d > input.dist[i] {
 				wins+=1
@@ -26,9 +28,9 @@ func main() {
 		results = append(results, wins)
 	}
 
-	result := 1
-	for _, num := range results {
-		result*=num
-	}
-	fmt.Printf("Margin of error = %v\n", result)
+	//result := 1
+	//for _, num := range results {
+	//	result*=num
+	//}
+	fmt.Printf("Margin of error = %v\n", results)
 }
